2994be925cf7a5f5f922566b28c9c3e67c4a9e3d
TianYuze <iamtyz@vip.qq.com>
2018-10-15 22:04:44 +0800

    1

--- test.py
+++ test.py
@@ -11,28 +11,28 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 
-x = np.linspace(0, 1, 51)
-y = np.linspace(0, 1, 51)
+x = np.linspace(0, 1, 101)
+y = np.linspace(0, 1, 101)
 data_x = []
 data_y = []
-hidden = 30
-learning_rate = 0.001
-epoch = 200000
+hidden = 45
+learning_rate = 0.1
+epoch = 100000
 
 for i in x:
     for j in y:
         data_x.append([i])
         data_y.append([j])
 
-x_in = tf.constant(data_x)
-y_in = tf.constant(data_y)
+x_in = tf.constant(data_x, dtype=tf.float32)
+y_in = tf.constant(data_y, dtype=tf.float32)
 
-w0x = tf.Variable(tf.random_normal(shape=[1, hidden], stddev=1))
-w0y = tf.Variable(tf.random_normal(shape=[1, hidden], stddev=1))
-b0x = tf.Variable(tf.random_normal(shape=[1, hidden], stddev=1))
-b0y = tf.Variable(tf.random_normal(shape=[1, hidden], stddev=1))
-w1 = tf.Variable(tf.random_normal(shape=[hidden, 1], stddev=1))
-b1 = tf.Variable(tf.random_normal(shape=[1, 1], stddev=1))
+w0x = tf.Variable(tf.random_normal(shape=[1, hidden], stddev=1), dtype=tf.float32)
+w0y = tf.Variable(tf.random_normal(shape=[1, hidden], stddev=1), dtype=tf.float32)
+b0x = tf.Variable(tf.random_normal(shape=[1, hidden], stddev=1), dtype=tf.float32)
+b0y = tf.Variable(tf.random_normal(shape=[1, hidden], stddev=1), dtype=tf.float32)
+w1 = tf.Variable(tf.random_normal(shape=[hidden, 1], stddev=1), dtype=tf.float32)
+b1 = tf.Variable(tf.random_normal(shape=[1, 1], stddev=1), dtype=tf.float32)
 
 a = tf.sigmoid(tf.matmul(x_in, w0x) + b0x) + tf.sigmoid(tf.matmul(y_in, w0y) + b0y)
 net_out = tf.matmul(a, w1) + b1
@@ -44,36 +44,57 @@ dsigmoidh2 = tf.sigmoid(a) + dsigmoidh - 2 * tf.sigmoid(a) * dsigmoidh
 pd_x2 = (w0x * w0x) * (tf.transpose(w1) * dsigmoidh2)
 pd_y2 = (w0y * w0y) * (tf.transpose(w1) * dsigmoidh2)
 
-loss = tf.reduce_mean((x_in * (1 - x_in) * (y_in * (1 - y_in) * pd_y2 + (2 - 4 * y_in) * pd_y - 2 * net_out) + y_in *
+# global_step=tf.Variable(0, trainable=False)
+# learning_rate = tf.train.exponential_decay(1.0, global_step, 1000, 0.96, staircase=False)
+
+tral = tf.abs(x_in * (1 - x_in) * (y_in * (1 - y_in) * pd_y2 + (2 - 4 * y_in) * pd_y - 2 * net_out) + y_in *
                        (1 - y_in) * (x_in * (1 - x_in) * pd_x2 + (2 - 4 * x_in) * pd_x - 2 * net_out) - (np.pi ** 2) *
-                       y_in * tf.sin(np.pi * x_in)) ** 2)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+                       y_in * tf.sin(np.pi * x_in))
+loss = tf.reduce_sum(tral)
+
+optimizer = tf.train.AdamOptimizer(learning_rate)
 train = optimizer.minimize((loss))
 
 init = tf.global_variables_initializer()
 
 sess = tf.Session()
 sess.run(init)
-
+'''
 for step in range(1, epoch + 1):
     sess.run(train)
     time = dt.datetime.now().isoformat()
     print(time, 'step:', step, 'loss:', sess.run(loss))
+'''
+sess.run(train)
+step = 1
+time = dt.datetime.now().isoformat()
+print(time, 'step:', step, 'loss:', sess.run(loss))
+temp = sess.run(loss)
+
+while sess.run(loss) > 3000:
+    sess.run(train)
+    step = step + 1
+    time = dt.datetime.now().isoformat()
+    print(time, 'step:', step, 'loss:', sess.run(loss))
+    if abs(sess.run(loss) - temp) < 0.00000001:
+        break
+    else:
+        temp = sess.run(loss)
 
 
 phi_t = x_in * (1 - x_in) * y_in * (1 - y_in) * net_out + y_in * tf.sin(np.pi * x_in)
 Z_t = sess.run(phi_t)
 
-'''
+
 fig_t = plt.figure()
+Y_t = sess.run(y_in)
 ax = fig_t.add_subplot(111, projection='3d')
 
 X_t = sess.run(x_in)
-Y_t = sess.run(y_in)
 Z_t = sess.run(phi_t)
 ax.scatter(X_t, Y_t, Z_t, c='g')
 plt.show()
-'''
+
 sess.close()
 
 #-----------------------------------------------------------------------------------
@@ -105,3 +126,4 @@ ax.scatter(X, Y, Z, c='g')
 plt.show()
 
 sess.close()
+print(max(abs(Z)))
