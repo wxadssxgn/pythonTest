658dd5d4c1c839c4a7fd8e7538ddf1b6fb7d6f15
TianYuze <iamtyz@vip.qq.com>
2018-11-27 11:06:36 +0800

    1

--- TensorflowTest.py
+++ TensorflowTest.py
@@ -1,6 +1,4 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*- 
-
+# -*- coding: utf-8 -*-
 
 import tensorflow as tf
 import numpy as np
@@ -15,7 +13,7 @@ x = np.linspace(0, 1, 101)
 y = np.linspace(0, 1, 101)
 data_x = []
 data_y = []
-hidden = 1000
+hidden = 45
 learning_rate = 0.1
 epoch = 100
 
@@ -44,9 +42,6 @@ dsigmoidh2 = tf.sigmoid(a) + dsigmoidh - 2 * tf.sigmoid(a) * dsigmoidh
 pd_x2 = (w0x * w0x) * (tf.transpose(w1) * dsigmoidh2)
 pd_y2 = (w0y * w0y) * (tf.transpose(w1) * dsigmoidh2)
 
-# global_step=tf.Variable(0, trainable=False)
-# learning_rate = tf.train.exponential_decay(1.0, global_step, 1000, 0.96, staircase=False)
-
 tral = tf.abs(x_in * (1 - x_in) * (y_in * (1 - y_in) * pd_y2 + (2 - 4 * y_in) * pd_y - 2 * net_out) + y_in *
                        (1 - y_in) * (x_in * (1 - x_in) * pd_x2 + (2 - 4 * x_in) * pd_x - 2 * net_out) - (np.pi ** 2) *
                        y_in * tf.sin(np.pi * x_in))
@@ -59,12 +54,7 @@ init = tf.global_variables_initializer()
 
 sess = tf.Session()
 sess.run(init)
-'''
-for step in range(1, epoch + 1):
-    sess.run(train)
-    time = dt.datetime.now().isoformat()
-    print(time, 'step:', step, 'loss:', sess.run(loss))
-'''
+
 sess.run(train)
 step = 1
 time = dt.datetime.now().isoformat()
@@ -72,6 +62,7 @@ print(time, 'step:', step, 'loss:', sess.run(loss))
 temp = sess.run(loss)
 
 while sess.run(loss) > 300000:
+    
     sess.run(train)
     step = step + 1
     time = dt.datetime.now().isoformat()
@@ -96,34 +87,3 @@ ax.scatter(X_t, Y_t, Z_t, c='g')
 plt.show()
 
 sess.close()
-
-#----------------------------------------------------------------------------------
-
-data_x = []
-data_y = []
-
-for i in x:
-    for j in y:
-        data_x.append([i])
-        data_y.append([j])
-
-x_in = tf.constant(data_x)
-y_in = tf.constant(data_y)
-phi_a = (1 / (tf.exp(np.pi) - tf.exp(-np.pi))) * tf.sin(np.pi * x_in) * (tf.exp(np.pi * y_in) - tf.exp(-np.pi * y_in))
-
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-X = sess.run(x_in)
-Y = sess.run(y_in)
-Z = -(sess.run(phi_a) - Z_t)
-ax.scatter(X, Y, Z, c='g')
-plt.show()
-
-sess.close()
-print(max(abs(Z)))
--- anEasyTorchExample.py
+++ anEasyTorchExample.py
@@ -6,6 +6,58 @@ import numpy as np
 from torch import nn, optim
 from torch.autograd import Variable
 import torch.nn.functional as F
+'''
+class MLP(nn.Module):
+    def __init__(self, in_dim, n_hidden_1, n_hidden_2,n_hidden_3 , out_dim):
+        super(MLP, self).__init__()
+        self.layer1 = nn.Sequential(nn.Linear(in_dim, n_hidden_1), nn.ReLU(True))
+        self.layer2 = nn.Sequential(nn.Linear(n_hidden_1, n_hidden_2), nn.ReLU(True))
+        self.layer3 = nn.Sequential(nn.Linear(n_hidden_2, n_hidden_3), nn.ReLU(True))
+        self.layer4 = nn.Sequential(nn.Linear(n_hidden_3, out_dim))
 
-a = nn.Conv2d(2, 3, 4, 1)
-print(a)
+        def forward(self, temp):
+            temp = self.layer1(temp)
+            temp = self.layer2(temp)
+            temp = self.layer3(temp)
+            temp = self.layer4(temp)
+            return temp
+
+learning_rate = 1e-2
+epoch = 200
+
+net = MLP(2, 50, 50, 50, 1)
+criterion = nn.MSELoss(size_average=False)
+optimizer = optim.SGD(net.parameters(), lr=1e-2, momentum=0.9)
+
+while loss.data[0] > 1:
+    data = np.random.rand(50, 2)
+    net_in = Variable(data, requires_grad=False)
+    opimizer.zero_grad()
+    net_out = net(net_in)
+    
+    loss = criterion(net_out, 0)
+'''
+'''
+net_in = Variable(data)
+net_out = net(net_in)
+target = 0
+criterion = nn.MSELoss()
+loss = criterion(net_out, target)
+net.zero_grad()
+'''
+dtype = torch.FloatTensor
+N, D_in, H, D_out = 64, 1000, 100, 10
+x = Variable(torch.randn(N, D_in).type(dtype), requires_grad=False)
+y = Variable(torch.randn(N, D_out).type(dtype), requires_grad=False)
+w1 = Variable(torch.randn(D_in, H).type(dtype), requires_grad=True)
+w2 = Variable(torch.randn(H, D_out).type(dtype), requires_grad=True)
+learning_rate = 1e-6
+for t in range(5000):
+    y_pred = x.mm(w1).clamp(min=0).mm(w2)
+    loss = (y_pred - y).pow(2).sum()
+    print(t, loss.data[0])
+    loss.backward()
+    w1.data -= learning_rate * w1.grad.data
+    w2.data-= learning_rate * w2.grad.data
+    w1.grad.data.zero_()
+    w2.grad.data.zero_()
