4661805eaeb9809ae2e68620c334db67013c0d1d
TianYuze <iamtyz@vip.qq.com>
2018-12-17 20:04:52 +0800

    1

--- txtRead.py
+++ txtRead.py
@@ -2,15 +2,39 @@
 # -*- coding: utf-8 -*- 
 
 import os
+import numpy as np
+
+path = "C:/Users/iamtyz/Desktop/data"
 
 
-def eachFile(filepath):
-    pathDir = os.listdir(filepath)
-    for s in pathDir:
-        newDir = os.path.join(filepath, s)
-        if os.path.isfile(newDir):
-            if os.path.splitext(newDir)[1] == ".txt":
-                os.readFile(newDir)
-                pass
-        else:
-            eachFile(newDir)
+def getfilename(path):
+    filelist = []
+    filepath = []
+    for filename in os.listdir(path):
+        if filename.endswith('.txt'):
+            tempfilename = path + '/' + filename
+            filepath.append(tempfilename)
+            filelist.append(filename.split('.')[0])
+    return filepath, filelist
+
+
+def getdata(filepath):
+    data = []
+    numfile = np.size(filepath)
+    for i in range(numfile):
+        f = open(filepath[i])
+        tempdata = []
+        line = f.readline()
+        while line:
+            num = list(map(float, line.split()))
+            tempdata.append(num)
+            line = f.readline()
+        f.close()
+        data.append(tempdata)
+    data = np.array(data)
+    return data
+
+
+filepath, filelist = getfilename(path)
+filedata = getdata(filepath)
+
